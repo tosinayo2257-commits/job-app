3aacd41b03b06f21b4dd22aa61a363b8017e0a89
tosinayo2257-commits <tosinayo2257@gmail.com>
2026-05-27 17:14:31 +0100

    update

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,11 +1,13 @@
 import { headers } from "next/headers";
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import { MongoClient } from "mongodb";
+
 import { redirect } from "next/navigation";
 import { initializeUserBoard } from "../init-user-board";
+import connectDB from "../db";
 
-const client = new MongoClient(process.env.MONGODB_URI!);
+const mongooseInstance = await connectDB();
+const client = mongooseInstance.connection.getClient();
 const db = client.db();
 
 export const auth = betterAuth({
